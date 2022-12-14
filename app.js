6b01cb3bf015e1649caf5aed0e2f3a17ae46d682
TitiaC62 <laetitia.crombet@gmail.com>
2022-12-14 22:06:52 +0100

    add style and effect btn

--- app.js
+++ app.js
@@ -45,7 +45,11 @@ function deleteCheck(e){
 
     if(item.classList[0] === "trash-btn"){
         const todo = item.parentElement;
-        todo.remove();
+        todo.classList.add("fall");
+        todo.addEventListener("transitionend", function() {
+            todo.remove();
+        })
+        
     }
     
     //CHECK MARK
